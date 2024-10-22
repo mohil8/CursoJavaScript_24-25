31fc6f664c5a64dc1c236156c40a79aa5cfb64cd
Mohi Essbai <mes-sbai01@educarex.es>
2024-10-22 12:38:23 +0200

    Cambios

---
+++ JuegoConValida/juego.js
@@ -0,0 +1,26 @@
+window.addEventListener('load',empezar);
+
+function empezar(){
+
+//Identifico bienvenida 
+const bienvenida = document.querySelector('#bienvenida');
+//Identificamos temporizador
+const tempo = document.querySelector('#temporizador');
+//Identificamos puntos
+const textPuntos = document.querySelector('#puntos');
+//Identificamo errores
+const textErrores = document.querySelector('#errores');
+//Identificamos suma
+const suma = document.querySelector('#suma');
+//Identificamo boton
+const boton = document.querySelector('input')
+
+let tiempo = 60;
+let puntos = 0;
+let errores= 0;
+
+let intervalo = setInterval()
+
+
+
+}
